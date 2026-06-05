254eac5c274fc77d3e988ec3ea0d421cc7e10600
rizz-cloud <rh05@hotmail.co.uk>
2026-06-05 19:35:43 +0100

    updated variables

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,16 @@
 resource "aws_instance" "this" {
-  ami                     = "ami-0150189e4c09ffab5"
-  instance_type           = "t3.micro"
+  ami                     = var.ami-id # Variable reference to the AMI ID defined in variables.tf
+  instance_type           = var.instance_type # Variable reference to the instance type defined in variables.tf
   availability_zone       = "eu-west-2a"
+  
   # Add in other configuration options as needed
   # Some may be required, such as tags, key_name, etc.
   # Other options may be optional, such as user_data, monitoring, etc.
 }
 
 resource "aws_instance" "import" {
-  ami                     = "ami-0150189e4c09ffab5"
-  instance_type           = "t3.micro"
+  ami                     = var.ami-id
+  instance_type           = var.instance_type
   availability_zone       = "eu-west-2a"
 
   tags = {
--- provider.tf
+++ provider.tf
@@ -5,8 +5,17 @@ terraform {
       version = "6.48.0"
     }
   }
+  ## Backend configuration for storing Terraform state in an S3 bucket.
+  ## Run Terraform init to initialize the backend after uncommenting and configuring the block below.
+
+  # backend "s3" {
+  #   bucket = "terraform-state-bucket"
+  #   key    = "terraform.tfstate"
+  #   region = "eu-west-2"
+  # }
 }
 
 provider "aws" {
   # Configuration options
 }
+
